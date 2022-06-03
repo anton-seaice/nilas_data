6b977dc73e013b230823c8a0057f499335ea0fa3
anton-climate <anton.steketee@anu.edu.au>
2022-06-03 13:21:05 +1000

    Tidy up of sea ice conc climatology

---
+++ utils/climat.py
@@ -0,0 +1,22 @@
+import xarray as xr
+
+def climatology(da, climat_dates=[1981,2010]):
+    """ For a provided da, calculate the mean and std dev, based on the climatology years for this instance
+
+    """
+
+    climat_ds=xr.Dataset()
+
+    climat_ds['ave']=da.where(
+        (da.time.dt.year>=climat_dates[0])
+        *(da.time.dt.year<=climat_dates[1]), 
+        drop=True
+    ).groupby('time.month').mean()
+
+    climat_ds['st_dev']=da.where(
+        (da.time.dt.year>=climat_dates[0])
+        *(da.time.dt.year<=climat_dates[1]), 
+        drop=True
+    ).groupby('time.month').std()
+    
+    return climat_ds
