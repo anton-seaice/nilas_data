03b557190a3955ab82290912fffab2ad6ac6b938
anton-climate <anton.steketee@anu.edu.au>
2022-06-21 13:01:03 +1000

    Add aggregators to regular downloads

---
+++ processing/pbs_run_aggregators.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -P jk72
+#PBS -l storage=gdata/jk72
+#PBS -M anton.steketee@aad.gov.au
+#PBS -m abe
+#PBS -q normal
+#PBS -o /g/data/jk72/MIZ/aggregator_logs
+#PBS -e /g/data/jk72/MIZ/aggregator_logs
+#PBS -W umask=0022
+#PBS -l ncpus=24
+#PBS -l mem=90gb
+
+umask 0003
+
+source /g/data/jk72/as2285/miniconda3/etc/profile.d/conda.sh
+
+conda activate /g/data/jk72/as2285/miniconda3/envs/sea-ice-tracker
+
+echo "Starting artist aggregator"
+
+python3 /g/data/jk72/as2285/miz/processing/artist_aggregator.py --src /g/data/jk72/MIZ/Bremen/netcdf/ --dest /g/data/jk72/MIZ/processed/
