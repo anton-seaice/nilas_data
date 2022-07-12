33131fe357afde3efc37421df6a55acd68a0c330
anton-climate <anton.steketee@anu.edu.au>
2022-07-12 15:05:27 +1000

    Minor tweaks to downloads

--- processing/pbs_download.sh
+++ processing/pbs_download.sh
@@ -2,11 +2,13 @@
 #PBS -P jk72
 #PBS -l storage=gdata/jk72
 #PBS -M anton.steketee@aad.gov.au
-#PBS -m abe
+#PBS -m ae
 #PBS -q copyq
 #PBS -o /g/data/jk72/MIZ/dl_logs
 #PBS -e /g/data/jk72/MIZ/dl_logs
 #PBS -W umask=0022
+#PBS -l walltime=2:00:00
+
 
 #Note that this bash script using the PBS copy queue. For transferring files from the internet. If we want to run processing subsequently well need a different job in the normal queue.
 
@@ -25,3 +27,4 @@ source /g/data/jk72/as2285/miz/processing/download_daily.sh
 # create monthly aggregated data 
 
 qsub /g/data/jk72/as2285/miz/processing/pbs_run_aggregators.sh
+
--- processing/pbs_run_aggregators.sh
+++ processing/pbs_run_aggregators.sh
@@ -2,13 +2,14 @@
 #PBS -P jk72
 #PBS -l storage=gdata/jk72
 #PBS -M anton.steketee@aad.gov.au
-#PBS -m abe
+#PBS -m ae
 #PBS -q normal
 #PBS -o /g/data/jk72/MIZ/aggregator_logs
 #PBS -e /g/data/jk72/MIZ/aggregator_logs
 #PBS -W umask=0022
-#PBS -l ncpus=24
-#PBS -l mem=90gb
+#PBS -l ncpus=16
+#PBS -l mem=48gb
+#PBS -l walltime=2:00:00
 
 umask 0003
 
@@ -19,3 +20,4 @@ conda activate /g/data/jk72/as2285/miniconda3/envs/sea-ice-tracker
 echo "Starting artist aggregator"
 
 python3 /g/data/jk72/as2285/miz/processing/artist_aggregator.py --src /g/data/jk72/MIZ/Bremen/netcdf/ --dest /g/data/jk72/MIZ/processed/
+
