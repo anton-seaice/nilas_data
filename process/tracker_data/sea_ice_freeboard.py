9e64d60beaaf2c8c499b9c543d792d95404fe249
Sean <sean.chua@ga.gov.au>
2023-09-26 16:04:22 +1000

    linting and docstrings for freeboard processing

--- process/tracker_data/sea_ice_freeboard.py
+++ process/tracker_data/sea_ice_freeboard.py
@@ -2,20 +2,17 @@
 
 #From the list of provided h5 files, generate geotifs of sea ice freeboard
 
-#directory paths
-_work_dir='/g/data/gv90/sc0554/miz/'
-_data_dir='/g/data/gv90/P6_data/'
-
 import h5py
 import pandas as pd
 import xarray as xr
 from pathlib import Path
-import sys
-import glob
 import os
 import odc.geo.xr
-sys.path.append('/g/data/jk72/sc0554/dea-notebooks/Tools/dea_tools/')
 from datacube.utils.cog import write_cog
+
+#directory paths
+_work_dir='/g/data/gv90/sc0554/miz/'
+_data_dir='/g/data/gv90/P6_data/'
 
 def paths_to_datetimeindex(paths, string_slice=(0, 10), form='%Y%m'):
     """
@@ -42,14 +39,29 @@ def paths_to_datetimeindex(paths, string_slice=(0, 10), form='%Y%m'):
 def create_filelist(input_path, file_pattern, string_slice=(9, 17)):
     """
     Create a sorted list of files from a directory with a given extension
+
+        Parameters
+    ----------
+    input_path : str
+        The path to the directory containing the files.
+    file_pattern : str
+        The file pattern to match. Only files matching this pattern will be included in the list.
+    string_slice : tuple, optional
+        An optional tuple giving the start and stop position that contains the time information in the provided paths.
+        These are applied to the basename (i.e. file name) in each path, not the path itself. Defaults to (9, 17).
+
+    Returns
+    -------
+    list
+        A sorted list of file paths matching the given pattern in the specified directory.
     """
-    filelist=[]
+    files=[]
     print("Finding files")
     for file in Path(input_path).rglob(file_pattern):
-        filelist.append(file)
-    filelist = sorted(filelist, key=lambda i: int(os.path.splitext(os.path.basename(i)[slice(*string_slice)])[0]))
-    print(f"Processing {len(filelist)} files")
-    return filelist
+        files.append(file)
+    sorted_files = sorted(files, key=lambda i: int(os.path.splitext(os.path.basename(i)[slice(*string_slice)])[0]))
+    print(f"Processing {len(sorted_files)} files")
+    return sorted_files
 
 print("Creating file list")
 
@@ -85,7 +97,7 @@ ds = ds.odc.assign_crs('EPSG:3031')
 print('Writing Cogs')
 
 for time in ds.time:
-     write_cog(
+    write_cog(
         ds['mean_fb'].sel(time=time),
         f'{_data_dir}/tracker_data/ATL20_cog/ATL20_monthly_fb_{time.dt.year.values}_{time.dt.month.values}.tiff',
         overwrite=True
